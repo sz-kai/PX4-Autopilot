10fc62bfdff91522115594b12ba45d15a639bf14
kai <1747098083@qq.com>
2025-12-23 14:30:33 +0800

    feat(examples): 添加px4_my_simple_app示例应用
    新增px4_my_simple_app示例应用，包含Kconfig和CMakeLists配置文件
    移除原px4_simple_app目录下的冗余文件

--- src/examples/px4_my_simple_app/px4_my_simple_app.c
+++ src/examples/px4_my_simple_app/px4_my_simple_app.c
@@ -38,7 +38,20 @@
  * @author Example User <mail@example.com>
  */
 
+// #include <px4_platform_common/px4_config.h>
 #include <px4_platform_common/log.h>
+// #include <px4_platform_common/tasks.h>
+// #include <px4_platform_common/posix.h>
+// #include <unistd.h>
+// #include <stdio.h>
+// #include <poll.h>
+// #include <string.h>
+// #include <math.h>
+
+// #include <uORB/uORB.h>
+// #include <uORB/topics/vehicle_acceleration.h>
+// #include <uORB/topics/vehicle_attitude.h>
+
 
 __EXPORT int px4_my_simple_app_main(int argc, char *argv[]);
 
