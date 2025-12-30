eb6528cf89c303971d24b8dfe5e1aa36da846a02
kai <1747098083@qq.com>
2025-12-30 17:07:37 +0800

    feat(px4_my_simple_app): 更新示例应用，添加数据订阅和发布功能

--- src/examples/px4_my_simple_app/px4_my_simple_app.c
+++ src/examples/px4_my_simple_app/px4_my_simple_app.c
@@ -70,102 +70,112 @@ int px4_my_simple_app_main(int argc, char *argv[])
 }
 #endif
 
-
 #if 1
-#include <px4_platform_common/px4_config.h>
-#include <px4_platform_common/log.h>
-#include <px4_platform_common/tasks.h>
-#include <px4_platform_common/posix.h>
-#include <unistd.h>
+
+/****************************************************************************
+ *
+ *   Copyright (c) 2012-2019 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+// #include <px4_platform_common/px4_config.h>
 #include <stdio.h>
-#include <poll.h>
 #include <string.h>
-#include <math.h>
+
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/posix.h>
 
 #include <uORB/uORB.h>
 #include <uORB/topics/sensor_combined.h>
 #include <uORB/topics/vehicle_attitude.h>
 
+__EXPORT void px4_my_simple_app_main(int argc, char *argv[]);
 
-__EXPORT int px4_my_simple_app_main(int argc, char *argv[]);
-
-int px4_my_simple_app_main(int argc, char *argv[])
+void px4_my_simple_app_main(int argc, char *argv[])
 {
 	PX4_INFO("Hello Sky!");
 
-	/* subscribe to sensor_combined topic */
-	/*sub:subscribe,fd:file descriptor,文件描述符*/
-	/*订阅数据*/
-	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
-	//int gps_sub_2 = orb_subscribe_multi(ORB_ID(vehicle_gps_position), 1);
-	/* limit the update rate to 5 Hz */
-	/*设置更新率为5Hz*/
-	orb_set_interval(sensor_sub_fd, 200);
 
-	/* advertise attitude topic */
-	struct vehicle_attitude_s att;
-	memset(&att, 0, sizeof(att));
-	/*广播vehicle_attitude数据，在发布数据前需要先广播*/
-	orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
+	/* 1. 订阅数据,获取文件描述符 */
+	int sensor_combined_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
 
-	/* one could wait for multiple topics with this technique, just using one here */
+	if (sensor_combined_sub_fd < 0) {
+		PX4_ERR("orb_subscribe failed - sensor_combined");
+		return;
+	}
+
 	px4_pollfd_struct_t fds[] = {
-		{ .fd = sensor_sub_fd,   .events = POLLIN },
-		/* there could be more file descriptors here, in the form like:
-		 * { .fd = other_sub_fd,   .events = POLLIN },
-		 */
+		{.fd = sensor_combined_sub_fd, .events = POLLIN},
+		/* 可以同时监视多个主题 */
+		/* {.fd = ..., .events = POLLIN} */
 	};
 
-	int error_counter = 0;
+	/* 2. 广播主题 */
+	struct vehicle_attitude_s att;
+	memset(&att, 0, sizeof(att));
+	orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
 
-	for (int i = 0; i < 5; i++) {
-		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+	/* 3. 等待订阅数据更新 */
+	int counter = 0;
+
+	for (counter = 0; counter < 10; counter++) { // 读取10次数据
 		int poll_ret = px4_poll(fds, 1, 1000);
 
-		/* handle the poll result */
 		if (poll_ret == 0) {
-			/* this means none of our providers is giving us data */
-			PX4_ERR("Got no data within a second");
+			PX4_WARN("No data within a second");
+			return;
 
+
+			
 		} else if (poll_ret < 0) {
-			/* this is seriously bad - should be an emergency */
-			if (error_counter < 10 || error_counter % 50 == 0) {
-				/* use a counter to prevent flooding (and slowing us down) */
-				PX4_ERR("ERROR return value from poll(): %d", poll_ret);
-			}
-
-			error_counter++;
+			PX4_ERR("px4_poll failed");
+			return;
 
 		} else {
-
 			if (fds[0].revents & POLLIN) {
-				/* obtained data for the first file descriptor */
+				/* 4. 读取数据 */
 				struct sensor_combined_s raw;
-				/* copy sensors raw data into local buffer */
-				orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+				orb_copy(ORB_ID(sensor_combined), sensor_combined_sub_fd, &raw);
 				PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f",
 					 (double)raw.accelerometer_m_s2[0],
 					 (double)raw.accelerometer_m_s2[1],
 					 (double)raw.accelerometer_m_s2[2]);
-
-				/* set att and publish this information for other apps
-				 the following does not have any meaning, it's just an example
-				*/
-				att.q[0] = raw.accelerometer_m_s2[0];
-				att.q[1] = raw.accelerometer_m_s2[1];
-				att.q[2] = raw.accelerometer_m_s2[2];
-
+				/* 5. 发布姿态数据 */
+				att.q[0] = raw.gyro_rad[0];
+				att.q[1] = raw.gyro_rad[1];
+				att.q[2] = raw.gyro_rad[2];
 				orb_publish(ORB_ID(vehicle_attitude), att_pub, &att);
 			}
 
-			/* there could be more file descriptors here, in the form like:
-			 * if (fds[1..n].revents & POLLIN) {}
-			 */
 		}
 	}
-
-	PX4_INFO("exiting");
-
-	return 0;
 }
+
 #endif
